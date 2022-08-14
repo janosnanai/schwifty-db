a8df0f53f725ce6cf13da1221e0bd298909f500f
Janos Nanai <janos.nanai@gmail.com>
2022-08-14 15:23:24 +0200

    added infinite scroll feature

--- hooks/infinite-scroll-hook.ts
+++ hooks/infinite-scroll-hook.ts
@@ -1,4 +1,28 @@
 // polyfill
+// @ts-ignore
 import "intersection-observer";
 
-function useInfiniteQuery() {}
+import type { RefObject } from "react";
+
+import { useEffect, useRef } from "react";
+
+export function useInfiniteScroll(callback: Function) {
+  const sentryRef: RefObject<any> = useRef();
+  useEffect(() => {
+    if (!sentryRef.current) return;
+    const observer = new IntersectionObserver((entries) => {
+      if (!entries[0].isIntersecting) return;
+      callback();
+      console.log(entries);
+    });
+    observer.observe(sentryRef.current);
+    console.log("observing " + typeof sentryRef.current);
+
+    return () => {
+      observer.disconnect();
+      console.log("observer disconnect");
+    };
+  }, [sentryRef, callback]);
+
+  return sentryRef;
+}
--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -7,6 +7,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 
 import CharacterCard from "../../components/card/character-card";
 import { getManyCharactersQueryFn } from "../../lib/api/query-functions";
+import { useInfiniteScroll } from "../../hooks/infinite-scroll-hook";
 
 const CharactersPage: NextPage = () => {
   const [filter, setFilter] = useState<FilterCharacter>({});
@@ -20,6 +21,11 @@ const CharactersPage: NextPage = () => {
         getNextPageParam: (lastPage, _pages) => lastPage.characters?.info?.next,
       }
     );
+
+  const sentryRef = useInfiniteScroll(() => {
+    console.log("intersection happened");
+    fetchNextPage({ cancelRefetch: false });
+  });
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -64,11 +70,15 @@ const CharactersPage: NextPage = () => {
         )}
       </div>
       <div className="flex flex-wrap justify-center">
+        {/* TODO: destroy this double map! */}
         {data?.pages.map((page) =>
           page.characters?.results?.map((c) => (
             <CharacterCard key={"c" + c!.id} character={c!} />
           ))
         )}
+      </div>
+      <div className="w-12 bg-red-500 text-white" ref={sentryRef}>
+        <p>sentry element</p>
       </div>
     </>
   );
