0fe64690f3344d6f1e96defc69a56d8e44914b90
Janos Nanai <janos.nanai@gmail.com>
2022-08-23 14:44:20 +0200

    fix: to-top-button not entiry area clickable

--- components/ui/to-top-button.tsx
+++ components/ui/to-top-button.tsx
@@ -2,11 +2,13 @@ import { ChevronDoubleUpIcon } from "@heroicons/react/outline";
 
 function ToTopButton({ className }: { className?: string }) {
   return (
-    <div className={`w-11 h-11 rounded-full bg-emerald-500 hover:bg-emerald-400 ${className}`}>
-      <a href="#page-top">
+    <a href="#page-top">
+      <div
+        className={`w-11 h-11 rounded-full bg-emerald-500 hover:bg-emerald-400 ${className}`}
+      >
         <ChevronDoubleUpIcon className="w-6 h-6 mx-auto mt-[9px] text-slate-800" />
-      </a>
-    </div>
+      </div>
+    </a>
   );
 }
 
