f1b9f540685df1552fe3eb146aa20a4fbf6d53bc
Janos Nanai <janos.nanai@gmail.com>
2022-08-31 07:27:47 +0200

    minor style tweaks

--- components/card/episode-card.tsx
+++ components/card/episode-card.tsx
@@ -20,18 +20,18 @@ function EpisodeCard({
         className="group m-1.5 border-0 rounded-lg shadow shadow-emerald-400/50 border-emerald-400 hover:shadow-lg hover:m-1 hover:border-2 hover:shadow-emerald-400/30 transition ease-out overflow-hidden"
       >
         <article className="flex flex-col justify-between bg-zinc-100 group-hover:bg-zinc-50 dark:bg-zinc-900 dark:group-hover:bg-zinc-800 h-48 transition ease-out w-60">
-          <div className="flex flex-col justify-center h-1/2 w-full px-2 py-0 shadow-sm bg-zinc-50/75 group-hover:bg-white/75 dark:bg-black/75 dark:group-hover:bg-zinc-900/75 transition ease-out backdrop-blur rounded">
+          <div className="flex flex-col justify-center h-1/2 w-full px-2 py-0 shadow-sm bg-zinc-50/75 group-hover:bg-white/75 dark:bg-black/75 dark:group-hover:bg-zinc-900/75 transition ease-out rounded">
             <h2 className="inline-block text-purple-800 dark:text-purple-500 text-2xl font-bold tracking-tight leading-tight">
               {episode.name}
             </h2>
           </div>
-          <div className="flex items-center h-12 p-1 gap-2 text-sm text-zinc-700 dark:text-zinc-300 tracking-wide">
+          <div className="flex items-center h-12 px-2 py-1 gap-2 text-sm text-zinc-700 dark:text-zinc-300 tracking-wide">
             <div className="border-r-2 border-zinc-300 dark:border-zinc-700 pr-2">
               <FilmIcon className="w-6 h-6 text-purple-400" />
             </div>
             <p>{episode.episode || FALLBACK_PROP_TEXT}</p>
           </div>
-          <div className="flex items-center h-12 p-1 gap-2 text-sm text-zinc-700 dark:text-zinc-300 tracking-wide">
+          <div className="flex items-center h-12 px-2 py-1 gap-2 text-sm text-zinc-700 dark:text-zinc-300 tracking-wide">
             <div className="border-r-2 border-zinc-300 dark:border-zinc-700 pr-2">
               <CalendarDaysIcon className="w-6 h-6 text-purple-400" />
             </div>
